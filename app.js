a223148d1a65ec234ad5c55f0be0e63f1639b81a
Rasloong <jorlu.arles@gmail.com>
2021-02-02 16:55:38 -0500

    tipos de objetos y su uso

--- app.js
+++ app.js
@@ -9,4 +9,15 @@
     console.log(mensaje);
     mensaje = 'hola no me perdi';
     console.log(mensaje);
+    var msj = 'hola';
+    var numero = 1243;
+    var boleano = true; //false
+    var hoy = new Date();
+    var any;
+    any = msj;
+    any = numero;
+    any = boleano;
+    any = hoy;
+    var spiderman = { nombre: 'Peter', edad: 30 };
+    spiderman = { nombre: 'miles', edad: 14 };
 })();
--- app.ts
+++ app.ts
@@ -10,5 +10,18 @@
    console.log(mensaje);
    mensaje='hola no me perdi';
    console.log(mensaje);
+   let msj:string='hola';
+   let numero=1243;
+   let boleano=true;//false
+   let hoy:Date=new Date();
+   let any;
+   
+   any=msj;
+   any=numero;
+   any=boleano;
+   any=hoy;
+
+   let spiderman={nombre:'Peter',edad:30};
+   spiderman={nombre:'miles',edad:14};
 })();
 
