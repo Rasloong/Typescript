3a47155f516f9c1bf879df68b6b0a1c3c87e2dd8
Rasloong <jorlu.arles@gmail.com>
2021-02-02 16:36:09 -0500

    generacion automatica primer js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 "use strict";
 (function () {
+    function saludar(nombre) {
+        console.log('Hola ' + nombre);
+    }
+    var wolverine = { nombre: 'Logan' };
+    saludar(wolverine.nombre);
 })();
