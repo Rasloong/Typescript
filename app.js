20fae2d773c556387bff67418e7b032bf1d6a1d8
Rasloong <jorlu.arles@gmail.com>
2021-02-02 16:37:41 -0500

    comentario a linea importante

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+//aunque en ts marca error por falta de linea en js corre perfectamente
 (function () {
     function saludar(nombre) {
         console.log('Hola ' + nombre);
--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
-
+//aunque en ts marca error por falta de linea en js corre perfectamente
 (function(){
    function saludar(nombre){
     console.log('Hola '+nombre);
+    
    }
    const wolverine={nombre:'Logan'}
    saludar(wolverine.nombre);
